7719e02faa51a75a3616eb9699cee6c97f82a716
doVa <doVaris@users.noreply.github.com>
2021-06-06 11:22:13 +0300

    Fusion Charts API setup

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -7,7 +7,29 @@ const Repos = () => {
   const { repos } = React.useContext(GithubContext);
   console.log(repos);
 
-  return <ExampleChart />;
+  // STEP 2 - Chart Data
+  const chartData = [
+    {
+      label: 'HTML',
+      value: '13',
+    },
+    {
+      label: 'CSS',
+      value: '23',
+    },
+    {
+      label: 'Javascript',
+      value: '80',
+    },
+  ];
+
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        <ExampleChart data={chartData} />;
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
