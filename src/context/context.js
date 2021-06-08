dcd2c85d17cd37107b820bc809215a49d3907314
doVa <doVaris@users.noreply.github.com>
2021-06-08 16:18:19 +0300

    Search User setup

--- src/context/context.js
+++ src/context/context.js
@@ -19,6 +19,24 @@ const GithubProvider = ({ children }) => {
   const [requests, setRequests] = useState(0);
   const [loading, setIsLoading] = useState(false);
 
+  // error
+  const [error, setError] = useState({ show: false, msg: '' });
+
+  const searchGithubUser = async user => {
+    toggleError();
+    // setLoading(true);
+    const response = await axios(`${rootUrl}/users/${user}`).catch(err =>
+      console.log(err)
+    );
+    console.log(response);
+    if (response) {
+      setGithubUser(response.data);
+      // more logic here
+    } else {
+      toggleError(true, 'there is no user with that username');
+    }
+  };
+
   // check rate
   const checkRequests = () => {
     axios(`${rootUrl}/rate_limit`)
@@ -29,17 +47,30 @@ const GithubProvider = ({ children }) => {
         setRequests(remaining);
 
         if (remaining === 0) {
-          // throw an error
+          toggleError(true, 'sorry, you have exeded your hourly rate limit!');
         }
       })
       .catch(err => console.log(err));
   };
+
+  function toggleError(show = false, msg = '') {
+    setError({ show, msg });
+  }
   // error
 
   useEffect(checkRequests, []);
 
   return (
-    <GithubContext.Provider value={{ githubUser, repos, followers, requests }}>
+    <GithubContext.Provider
+      value={{
+        githubUser,
+        repos,
+        followers,
+        requests,
+        error,
+        searchGithubUser,
+      }}
+    >
       {children}
     </GithubContext.Provider>
   );
